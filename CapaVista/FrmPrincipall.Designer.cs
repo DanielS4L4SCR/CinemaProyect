df735a7f294f69d69fa9982e47084ba3c50636b1
DanielS4L4SCR <luisda7781@hotmail.com>
2017-07-31 18:04:35 -0600

    cambios

--- CapaVista/FrmPrincipall.Designer.cs
+++ CapaVista/FrmPrincipall.Designer.cs
@@ -616,7 +616,6 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(2000, 1000);
             this.panel1.TabIndex = 10;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // panel8
             // 
